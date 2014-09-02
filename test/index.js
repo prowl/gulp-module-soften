2748252e450e744aa731f73b503e5e7cdbe7b382
Joshwa Fugett <jfugett27@gmail.com>
2014-09-02 16:17:20 -0700

    fix(build): Fixing build errors

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@
 var should = require('should'); //jshint ignore:line
 
 var soften = require('../lib/index');
-var stream = require('stream');
 
 var gulpMock = {};
 var task = null;
@@ -13,14 +12,7 @@ gulpMock.task = function(name, deps, func) {
 };
 
 var configMock = {
-  root: __dirname,
   soften: ['**/*.js', '**/*.json', '!./node_modules/**', '!./docs/**'],
-  src: ['**/*.js', '!./node_modules/**', '!./docs/**'],
-  specs: ['specs/**/*.js'],
-  statements_threshold: 90,
-  functions_threshold: 100,
-  branches_threshold: 100,
-  lines_threshold: 80
 };
 
 soften(gulpMock, configMock);
@@ -35,9 +27,9 @@ describe('Gulp Module Soften', function() {
   });
 
   it('Should run and throw an error', function(cb) {
-    try{
+    try {
       task();
-    } catch(e) {
+    } catch (e) {
       cb();
     }
   });
